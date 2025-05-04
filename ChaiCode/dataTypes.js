16b843132cbd900523f738f4699525a28360d7ad
PatilSridatta <patilsridatta@gmail.com>
2025-05-04 17:17:27 +0530

    Basics of javascript day1

---
+++ ChaiCode/dataTypes.js
@@ -0,0 +1,19 @@
+//when you use "use strict" newer version of JS is used.
+
+// alert("hello"); 
+//we are using nodejs not browser so it is giving error
+
+let a = 10;
+let b = "Sridatta";
+let c = 7896342n;
+let d = true;
+let e = null;
+let f = undefined;
+
+console.table([a,b,c,d,e,f]);
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
---
+++ ChaiCode/variables.js
@@ -0,0 +1,12 @@
+const accountId = 78965
+let accountEmail = "example@example.com"
+var accountPassword = "123456"
+accountCity = "Bidar"
+let accountState;
+
+accountEmail = "patil@mail.com"
+accountPassword = "789654"
+accountCity = "Bengaluru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+//prefer not to use var
